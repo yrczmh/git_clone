88d82052b1f007bd5a917a9305dc4ad82e53d1b1
yrczmh <64769464+yrczmh@users.noreply.github.com>
2020-11-27 14:45:36 +0800

    Update login.py

--- login.py
+++ login.py
@@ -1,1 +1,312 @@
-a = 10
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>数据南京软件开发二部 &mdash; 100% Free Fully Responsive HTML5 Template </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Free HTML5 Template by FREEHTML5"/>
+    <meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive"/>
+
+
+    <!-- Facebook and Twitter integration -->
+    <meta property="og:title" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:site_name" content=""/>
+    <meta property="og:description" content=""/>
+    <meta name="twitter:title" content=""/>
+    <meta name="twitter:image" content=""/>
+    <meta name="twitter:url" content=""/>
+    <meta name="twitter:card" content=""/>
+
+    <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,300' rel='stylesheet' type='text/css'>
+
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="css/animate.css">
+    <!-- Icomoon Icon Fonts-->
+    <link rel="stylesheet" href="css/icomoon.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Superfish -->
+    <link rel="stylesheet" href="css/superfish.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <!-- Modernizr JS -->
+    <script src="js/modernizr-2.6.2.min.js"></script>
+    <script src="js/echarts.min.js"></script>
+    <!-- FOR IE9 below -->
+    <!--[if lt IE 9]>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="fh5co-wrapper">
+    <div id="fh5co-page">
+        <div id="fh5co-header">
+            <header id="fh5co-header-section">
+                <div class="container">
+                    <div class="nav-header">
+                        <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle"><i></i></a>
+                        <h1 id="fh5co-logo"><a href="index.html">数据南京软件开发二部</a></h1>
+                        <!-- START #fh5co-menu-wrap -->
+                        <nav id="fh5co-menu-wrap" role="navigation">
+                            <ul class="sf-menu" id="fh5co-primary-menu">
+                                <li>
+                                    <a href="index.html">首页</a>
+                                </li>
+                                <li class="active"><a href="about.html">工具</a></li>
+                                <li><a href="blog.html">FT</a></li>
+                                <!-- <li><a href="contact.html">Contact</a></li> -->
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+
+        </div>
+
+
+        <div class="fh5co-hero fh5co-hero-2">
+            <div class="fh5co-overlay"></div>
+            <div class="fh5co-cover fh5co-cover_2 text-center" data-stellar-background-ratio="0.5"
+                 style="background-image: url(images/about-image.jpg);">
+                <div class="desc animate-box">
+                    <h2><strong>工具组产品使用情况</strong></h2>
+                    <span>Lovely Crafted by <a href="#" target="_blank" class="fh5co-site-name">FREEHTML5.co</a></span>
+                </div>
+            </div>
+        </div>
+        <!-- end:header-top -->
+        <div id="fh5co-about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+                        <h3>How we started</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores,
+                            perspiciatis accusamus asperiores sint consequuntur debitis.</p>
+                    </div>
+                </div>
+            </div>
+            <div align="center">
+                <h2>转发面工具下载</h2>
+
+                <table border="1" width="90%">
+                    <tbody>
+                    <tr>
+                        <th width="16%">工具链接</th>
+                        <th width="10%">版本号</th>
+                        <th>简介</th>
+                        <th width="10%">下载统计</th>
+                        <th width="10%">使用统计</th>
+                    </tr>
+
+                    <tr>
+                        <td><a href="diagdownload">转发面智能诊断工具下载</a></td>
+                        <td>{{ ver_diag }}</td>
+                        <td>转发面智能诊断工具同时支持SSP和NP5芯片，集抓包，统计，ftm表项打印于一体的综合诊断工具，方便故障定位和开发调试</td>
+                        <td>{{ download_statistic_diag }}</td>
+                        <td>{{ use_statistic_diag }}</td>
+                    </tr>
+                    <tr>
+                        <td><a href="mcplatedownload">McPlate工具下载</a></td>
+                        <td>{{ ver_mcplate }}</td>
+                        <td>MC-IDE 自研开发的一款高效的代码开发/代码阅读工具综合集成开发环境，类似于Eclipse的集成开发环境，可以便捷的集成多种不同类型芯片的代码工程</td>
+                        <td>{{ download_statistic_mcplate }}</td>
+                        <td>{{ use_statistic_mcplate }}</td>
+                    </tr>
+
+
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+        <!-- END fh5co-about -->
+        <div id="fh5co-content-section" class="fh5co-section-gray">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+                        <h3>转发面智能诊断工具详情使用统计</h3>
+                        <p>为统计用户使用情况，基于各项功能展开统计</p>
+                    </div>
+                </div>
+            </div>
+            <div id="main" style="width: 1920px;height:400px;text-align: center;"></div>
+
+        </div>
+    </div>
+    <!-- fh5co-content-section -->
+    <div id="fh5co-services-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2 text-center heading-section animate-box">
+                    <h3>What We Do</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit est facilis maiores, perspiciatis
+                        accusamus asperiores sint consequuntur debitis.</p>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-4 col-sm-4">
+                    <div class="services animate-box">
+                        <span><i class="icon-browser"></i></span>
+                        <h3>Web Development</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                            there live the blind texts.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="services animate-box">
+                        <span><i class="icon-mobile"></i></span>
+                        <h3>Mobile Apps</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                            there live the blind texts.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="services animate-box">
+                        <span><i class="icon-tools"></i></span>
+                        <h3>UX Design</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                            there live the blind texts.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="services animate-box">
+                        <span><i class="icon-video"></i></span>
+                        <h3>Video Editing</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                            there live the blind texts.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="services animate-box">
+                        <span><i class="icon-search"></i></span>
+                        <h3>SEO Ranking</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                            there live the blind texts.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-4">
+                    <div class="services animate-box">
+                        <span><i class="icon-cloud"></i></span>
+                        <h3>Cloud Based Apps</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,
+                            there live the blind texts.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END fh5co-services-section -->
+
+    <footer>
+        <div id="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-md-offset-3 text-center">
+                        <p class="fh5co-social-icons">
+                            <a href="#"><i class="icon-twitter2"></i></a>
+                            <a href="#"><i class="icon-facebook2"></i></a>
+                            <a href="#"><i class="icon-instagram"></i></a>
+                            <a href="#"><i class="icon-dribbble2"></i></a>
+                            <a href="#"><i class="icon-youtube"></i></a>
+                        </p>
+                        <p>Copyright 2016 Free Html5 <a href="#">数据南京软件开发二部</a>. All Rights Reserved. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+</div>
+<!-- END fh5co-page -->
+
+</div>
+<!-- END fh5co-wrapper -->
+
+<!-- jQuery -->
+
+
+<script src="js/jquery.min.js"></script>
+<!-- jQuery Easing -->
+<script src="js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js"></script>
+<!-- Stellar -->
+<script src="js/jquery.stellar.min.js"></script>
+<!-- Superfish -->
+<script src="js/hoverIntent.js"></script>
+<script src="js/superfish.js"></script>
+
+<!-- Main JS (Do not remove) -->
+<script src="js/main.js"></script>
+<script type="text/javascript">
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById('main'));
+
+
+    // 指定图表的配置项和数据
+    var option = {
+        color: ['#3398DB'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['报文解析', '微码抓包', '模块统计', '诊断函数', '表项打印', '丢包上送', 'Trace', '收发包抓包', '详细日志', '自动生成FT', '加载微码'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '使用次数',
+                type: 'bar',
+                barWidth: '60%',
+                data: [10, 52, 200, 334, 390, 330, 220, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    };
+
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+</script>
+</body>
+</html>
+
